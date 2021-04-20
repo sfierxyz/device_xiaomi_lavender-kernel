bd2fba0e23603a9d372a5d6c4fd1a8a5b09e74f6
DillerOFire <niktofe1@gmail.com>
2021-04-20 17:32:18 +1000

    initial headers

---
+++ sdm660/kernel-headers/linux/cifs/..install.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/linux/cifs/.install := /bin/bash ../scripts/headers_install.sh ./usr/include/linux/cifs ../include/uapi/linux/cifs ; /bin/bash ../scripts/headers_install.sh ./usr/include/linux/cifs ./include/generated/uapi/linux/cifs ; for F in ; do echo "$(pound)include <asm-generic/$$F>" > ./usr/include/linux/cifs/$$F; done; touch usr/include/linux/cifs/.install
---
+++ sdm660/kernel-headers/linux/hdlc/..install.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/linux/hdlc/.install := /bin/bash ../scripts/headers_install.sh ./usr/include/linux/hdlc ../include/uapi/linux/hdlc ioctl.h; /bin/bash ../scripts/headers_install.sh ./usr/include/linux/hdlc ./include/generated/uapi/linux/hdlc ; for F in ; do echo "$(pound)include <asm-generic/$$F>" > ./usr/include/linux/hdlc/$$F; done; touch usr/include/linux/hdlc/.install
